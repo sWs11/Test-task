8014d25b3113787cff356411422033e8424e30a0
Sasha <Lavrechko_sasha94@mail.ru>
2016-05-25 12:41:14 +0300

    Coment

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php 
+	global $host;
+	global $user;
+	global $password;
+	global $database;
+	
+
+	$host = 'localhost'; // адрес сервера 
+	$user = 'root'; // і'мя користувача
+	$password = ''; // пароль
+	$database = 'company'; //і'мя имя бази даних
+	
+	
+	
+?>
